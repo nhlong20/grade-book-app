70d49093f35f20360e4c91e788c6235fb8307e13
Hieu Pham <hieupham@kms-technology.com>
2021-11-02 10:56:55 +0700

    feat: mail module

---
+++ backend/src/mail/mail.service.ts
@@ -0,0 +1,9 @@
+import { MailerService } from '@nestjs-modules/mailer'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) { }
+
+  async sendMail() { }
+}

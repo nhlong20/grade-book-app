222070c71b445a072028de2eb108842b282b3027
nhlong20 <hoanglongmcs1@gmail.com>
2021-11-12 23:18:41 +0700

    fix: minor bug

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -49,11 +49,6 @@ const Home: NextPage = () => {
                 Tham gia lớp học
               </button>
             </div>
-            <Link href="/contribute">
-              <button className="py-2 px-4 font-semibold shadow-md text-black hover:text-green-700 mx-1">
-                Đăng nhập
-              </button>
-            </Link>
           </div>
 
         </main>

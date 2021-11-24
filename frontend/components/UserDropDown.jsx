4394aed614f3f5cb9292acbd9e765258d22eb71d
nhlong20 <hoanglongmcs1@gmail.com>
2021-11-24 14:14:57 +0700

    feat: add user dropdown and illlustration/

---
+++ frontend/components/UserDropDown.jsx
@@ -0,0 +1,73 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { signOut } from 'next-auth/client'
+import Link from 'next/link'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function UserDropDown({ userData }) {
+    return (
+        <Menu as="div" className="relative inline-block text-left">
+            <div>
+                <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                    <div className="flex items-center cursor-pointer">
+                        <p className="font-semibold">{userData.name}</p>
+                        <img
+                            src={"/team-1-800x800.jpg"}
+                            alt="..."
+                            className="w-8 h-8 rounded-full border-2 border-blueGray-50 shadow ml-2"
+                        ></img>
+                    </div>
+                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                        <Menu.Item>
+                            {({ active }) => (
+                                <Link href="/login">
+                                    <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                    >
+                                        Profile
+                                    </a>
+                                </Link>
+                            )}
+                        </Menu.Item>
+
+                        <Menu.Item>
+                            {({ active }) => (
+                                <button
+                                    type="button"
+                                    onClick={signOut}
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block w-full text-left px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Sign out
+                                </button>
+                            )}
+                        </Menu.Item>
+                    </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+    )
+}

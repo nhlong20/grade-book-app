5cd0d1870b5b3129b7b0902a1739c3a0001fd465
nhlong20 <hoanglongmcs1@gmail.com>
2021-12-01 15:41:46 +0700

    feat: login with google

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,30 +1,57 @@
 import { signIn } from 'next-auth/client'
 import { useRouter } from 'next/router'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import Link from 'next/link'
+import { notification } from 'antd'
 
 type FormData = {
   email: string
   password: string
 }
 
+const errorMapping: Record<string, string> = {
+  AccessDenied: 'Your account has not existed in the system yet!',
+  CredentialsSignin: 'Your provided credentials are not correct!',
+  Callback: 'System failure!',
+};
+
 export default function Login() {
   const { query } = useRouter()
   const { register, handleSubmit } = useForm<FormData>()
 
+  useEffect(() => {
+    if (!('error' in query)) return
+    notification.error({
+      message: errorMapping[query.error as string],
+    })
+  }, [query])
+
   const login = useCallback(
-    handleSubmit((data) => {
+    (data: Record<string, any>) => {
       signIn('login', {
         callbackUrl: (query.callbackUrl as string) || '/',
         ...data,
-      })
-    }),
-    [],
-  )
+      });
+    },
+    [query]
+  );
+
+  const loginWithGoogle = useCallback(() => {
+    signIn('google', {
+      callbackUrl: (query.callbackUrl as string) || '/',
+    });
+  }, [query]);
 
   return (
     <div className="min-h-screen grid w-full place-content-center">
-      <form onSubmit={login} noValidate>
+      <form
+        onSubmit={handleSubmit(login)}
+        className="w-96 rounded-md bg-white dark:bg-gray-600 inline-block p-8 shadow-md"
+        noValidate>
+        <h3 className="text-4xl font-bold text-blue-500 py-4 content-conter justify-center">
+          Login to <br /> GRADEBOOKS
+        </h3>
         <div className="mb-4">
           <label htmlFor="email" className="cf-label">
             Email
@@ -33,7 +60,12 @@ export default function Login() {
             type="text"
             id="email"
             className="cr-input w-full"
-            {...register('email')}
+            {...register('email', {
+              required: {
+                value: true,
+                message: 'Email is required',
+              },
+            })}
           />
         </div>
 
@@ -46,15 +78,38 @@ export default function Login() {
             autoComplete="currentpassword"
             id="pwd"
             className="cr-input w-full"
-            {...register('password')}
+            {...register('password', {
+              required: {
+                value: true,
+                message: 'Password is required',
+              },
+            })}
           />
         </div>
 
         <div className="mt-2">
-          <button type="submit" className="cr-button">
+          <button type="submit" className="cr-button w-full py-3 uppercase font-bold">
             Login
           </button>
         </div>
+        <div className="mt-2">
+          <button
+            type="submit"
+            className="w-full text-center py-3 rounded bg-red-500 text-white hover:bg-red-600 focus:outline-none my-1 uppercase font-bold"
+            onClick={loginWithGoogle}
+          >
+            {' '}
+            <i className="fab fa-google mr-2" />
+            Đăng nhập với Google
+          </button>
+
+        </div>
+        <p className="mt-2 flex justify-center">
+          Bạn chưa có tài khoản? {' '}
+          <Link href="/signup">
+            <span className="underline cursor-pointer">Đăng ký</span>
+          </Link>
+        </p>
       </form>
     </div>
   )
--- frontend/utils/service/user.ts
+++ frontend/utils/service/user.ts
@@ -5,6 +5,7 @@ import { API } from 'environment'
 export const getUser = (token?: string) => () =>
   axios.get<User>(API + '/user').then((r) => r.data)
 
-  
+
+
 export const updateUserInformation = (data: { name: string }) =>
-axios.patch<User>(API + '/api/user', data).then((res) => res.data)
+  axios.patch<User>(API + '/api/user', data).then((res) => res.data)

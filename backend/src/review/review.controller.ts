21e6bbd3491e376e1f2ce354349f19bfbaf28e65
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-01-16 14:28:19 +0700

    feat: review api route

--- backend/src/review/review.controller.ts
+++ backend/src/review/review.controller.ts
@@ -1,9 +1,56 @@
-import { Controller } from '@nestjs/common'
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { DTO } from '@/type'
+import { AuthRequest } from '@/utils/interface'
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  Req,
+} from '@nestjs/common'
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
+import { ReviewService } from './review.service'
 
 @Controller('review')
 @ApiTags('review')
 @ApiBearerAuth('access-token')
 export class ReviewController {
-  constructor() {}
+  constructor(private service: ReviewService) {}
+
+  @Get(':id')
+  @ApiOperation({ summary: 'to get one review' })
+  getOneReview(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Req() req: AuthRequest,
+  ) {
+    return this.service.getOneReview(id, req)
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'to create review' })
+  create(@Body() dto: DTO.Review.CreateReview, @Req() req: AuthRequest) {
+    return this.service.createReview(dto, req)
+  }
+
+  @Put(':id/resovle')
+  @ApiOperation({ summary: 'to create review' })
+  resolve(@Param('id', ParseUUIDPipe) id: string, @Req() req: AuthRequest) {
+    return this.service.resolveReview(id, req)
+  }
+
+  @Post('comment')
+  @ApiOperation({ summary: 'to create comment' })
+  createComment(
+    @Body() dto: DTO.Comment.CreateComment,
+    @Req() req: AuthRequest,
+  ) {
+    return this.service.createComment(dto, req)
+  }
 }
